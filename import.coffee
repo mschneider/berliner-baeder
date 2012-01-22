cda295562f72fd9f4384e0009a99ef7dca3ff59b
M.Schneider <not.m.schneider@gmail.com>
2012-01-22 13:09:11 +0100

    improved import script

--- import.coffee
+++ import.coffee
@@ -1,21 +1,54 @@
 berlin = require './berlin.json'
+fs = require 'fs'
 request = require 'request'
 $ = require 'jQuery'
+_ = require 'underscore'
+_.str = require 'underscore.string'
+_.mixin _.str.exports()
 
 baseUrl = 'http://www.berlinerbaederbetriebe.de/'
 
 parseBath = (body) ->
-  $(body).find('div#content > p').each (index, p) ->
-    console.log $(p).text()
+  name = $(body).find('#content h1:first').text()
+  features = []
+  text = $(body).find('#content').text()
+  features.push '25-m-Becken' if _.str.include text, '25-m-Becken'
+  features.push '50-m-Becken' if _.str.include text, '50-m-Becken'
+  time_table = $(body).find('#content_ul > table:first')
+  bath =
+    'name': name,
+    'features': features,
+    'openingTimes': parseTimeTable time_table
 
-parseBathList = (body) ->
-  $(body).find('div#content > p > a').each (index, bathLink) ->
+parseBathList = (body, cb) ->
+  bathLinks = $(body).find('div#content > p > a')
+  result = []
+  returnResult = _.after bathLinks.length, () ->
+    cb(result)
+  bathLinks.each (index, bathLink) ->
     href = $(bathLink).attr 'href'
     request baseUrl + href, (error, response, body) ->
       if !error and response.statusCode == 200
-        console.log "found bath", href
+        result.push parseBath body
+        returnResult()
 
+parseTimeTable = (table) ->
+  result = {}
+  lastDay = undefined
+  $(table).find('tr').each (index, row) ->
+    [day, time, comment] = _.map $(row).find('td'), (node) ->
+      _.trim $(node).text()
+    day ||= lastDay
+    result[day] ||= {}
+    result[day][time] = comment
+    # save last day
+    lastDay = day
+  result
 
 request baseUrl + '24.html', (error, response, body) ->
   if !error and response.statusCode == 200
-    parseBathList body
+    parseBathList body, (baths) ->
+      console.log JSON.stringify baths
+      # fs.writeFile 'baths.json', JSON.stringify(baths), (err) ->
+      #  if (err) throw err
+      # console.log "Imported into baths.json"
--- import.js
+++ import.js
@@ -1,32 +1,76 @@
-var $, baseUrl, berlin, parseBath, parseBathList, request;
+var $, baseUrl, berlin, fs, parseBath, parseBathList, parseTimeTable, request, _;
 
 berlin = require('./berlin.json');
+
+fs = require('fs');
 
 request = require('request');
 
 $ = require('jQuery');
 
+_ = require('underscore');
+
+_.str = require('underscore.string');
+
+_.mixin(_.str.exports());
+
 baseUrl = 'http://www.berlinerbaederbetriebe.de/';
 
 parseBath = function(body) {
-  return $(body).find('div#content > p').each(function(index, p) {
-    return console.log($(p).text());
-  });
+  var bath, features, name, text, time_table;
+  name = $(body).find('#content h1:first').text();
+  features = [];
+  text = $(body).find('#content').text();
+  if (_.str.include(text, '25-m-Becken')) features.push('25-m-Becken');
+  if (_.str.include(text, '50-m-Becken')) features.push('50-m-Becken');
+  time_table = $(body).find('#content_ul > table:first');
+  return bath = {
+    'name': name,
+    'features': features,
+    'openingTimes': parseTimeTable(time_table)
+  };
 };
 
-parseBathList = function(body) {
-  return $(body).find('div#content > p > a').each(function(index, bathLink) {
+parseBathList = function(body, cb) {
+  var bathLinks, result, returnResult;
+  bathLinks = $(body).find('div#content > p > a');
+  result = [];
+  returnResult = _.after(bathLinks.length, function() {
+    return cb(result);
+  });
+  return bathLinks.each(function(index, bathLink) {
     var href;
     href = $(bathLink).attr('href');
     return request(baseUrl + href, function(error, response, body) {
       if (!error && response.statusCode === 200) {
-        console.log("found bath", href);
-        return parseBath(body);
+        result.push(parseBath(body));
+        return returnResult();
       }
     });
   });
 };
 
+parseTimeTable = function(table) {
+  var lastDay, result;
+  result = {};
+  lastDay = void 0;
+  $(table).find('tr').each(function(index, row) {
+    var comment, day, time, _ref;
+    _ref = _.map($(row).find('td'), function(node) {
+      return _.trim($(node).text());
+    }), day = _ref[0], time = _ref[1], comment = _ref[2];
+    day || (day = lastDay);
+    result[day] || (result[day] = {});
+    result[day][time] = comment;
+    return lastDay = day;
+  });
+  return result;
+};
+
 request(baseUrl + '24.html', function(error, response, body) {
-  if (!error && response.statusCode === 200) return parseBathList(body);
+  if (!error && response.statusCode === 200) {
+    return parseBathList(body, function(baths) {
+      return console.log(JSON.stringify(baths));
+    });
+  }
 });
